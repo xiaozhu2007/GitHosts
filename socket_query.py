3f0341509695f64f8659b05e227ea9c710ff99dc
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2022-04-20 16:43:03 +0800

    Update socket_query.py

--- socket_query.py
+++ socket_query.py
@@ -25,7 +25,6 @@ domains = [
     'git.io',                                   # 用途包括但不限于: Git.io短链接
     'allcontributors.org',                                   # 用途包括: All Contributors . org
     'user-images.githubusercontent.com',
-    'x-status.pages.dev',
 ]
 
 def gen_host():
@@ -44,11 +43,11 @@ def get_now_date_str(format_string="%Y-%m-%d %H:%M:%S"):#"%Y-%m-%d %H:%M:%S"
 def output_hosts():
     with open('hosts.txt', 'w') as f:
         f.write('```\n')
-        f.write('# GitHost Start \n')
+        f.write('# GitHosts Start \n')
         f.write('# Last update at %s (Beijing Time)\n'%(get_now_date_str()))
         for ip, domain in gen_host():
             f.write('%s %s\n'%(ip, domain))
-        f.write('# GitHost End \n')
+        f.write('# GitHosts End \n')
         f.write('```\n')
 if __name__ == '__main__':
     output_hosts()
