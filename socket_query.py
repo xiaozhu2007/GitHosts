abb5238bb0a16ffdd901ffef54c93f4065b2e268
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2022-06-29 18:19:40 +0800

    添加注释; 修改`北京时间`为`UTC+8`; 更改运行时
    添加注释; 修改`北京时间`为`UTC+8`; 更改运行时

--- socket_query.py
+++ socket_query.py
@@ -1,30 +1,29 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding:utf-8
  
 import socket
 from datetime import datetime, timedelta, timezone
 domains = [
-    'api.github.com',                         # Github API
-    'assets-cdn.github.com',               # Github CDN
-    'avatars.githubusercontent.com',
-    'avatars0.githubusercontent.com',
-    'camo.githubusercontent.com',                         # Github Camo 服务 
-    'cloud.githubusercontent.com',
-    'codeload.github.com',
-    'favicons.githubusercontent.com',
-    'gist.github.com',
-    'gist.githubusercontent.com',
-    'github.com',                              # 网站本体
-    'github.githubassets.com',
+    'api.github.com',                          # Github API
+    'assets-cdn.github.com',                   # Github CDN
+    'avatars.githubusercontent.com',           # Github 头像CDN
+    'avatars0.githubusercontent.com',          # Github 头像CDN
+    'camo.githubusercontent.com',              # Github Camo 服务 
+    'cloud.githubusercontent.com',             # Github 储存桶
+    'codeload.github.com',                     # Github 前端渲染组件库
+    'favicons.githubusercontent.com',          # Github 图标等资源
+    'gist.github.com',                         # Github Gists
+    'gist.githubusercontent.com',              # Github Gists CDN(Raw)
+    'github.com',                              # Github 源站
+    'github.githubassets.com',                 # Github Assets
     'marketplace-screenshots.githubusercontent.com',
-    'octocaptcha.com',                                      # 用途包括但不限于: 创建用户、组织、重设密码、验证真人时的验证码
-    'xiaozhu2007.github.io',                              # Github.io子域名
-    'raw.githubusercontent.com',
-    'repository-images.githubusercontent.com',
-    'uploads.github.com',                                   # 用途包括但不限于: Release附件上传
-    'git.io',                                   # 用途包括但不限于: Git.io短链接
-    'allcontributors.org',                                   # 用途包括: All Contributors . org
-    'user-images.githubusercontent.com',
+    'octocaptcha.com',                         # 用途包括但不限于: 创建用户、组织、重设密码、验证真人时的验证码
+    'xiaozhu2007.github.io',                   # Github.io子域名
+    'raw.githubusercontent.com',               # Github Raw
+    'repository-images.githubusercontent.com', # Github 仓库缩略图
+    'uploads.github.com',                      # 用途包括但不限于: Release附件上传
+    'git.io',                                  # 用途包括但不限于: Git.io短链接
+    'user-images.githubusercontent.com',       # Github 用户上传的图片
 ]
 
 def gen_host():
@@ -44,7 +43,7 @@ def output_hosts():
     with open('hosts.txt', 'w') as f:
         f.write('```\n')
         f.write('# GitHosts Start \n')
-        f.write('# Last update at %s (Beijing Time)\n'%(get_now_date_str()))
+        f.write('# Last update at %s (UTC+8)\n'%(get_now_date_str()))
         for ip, domain in gen_host():
             f.write('%s %s\n'%(ip, domain))
         f.write('# GitHosts End \n')
